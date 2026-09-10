6951879e54e713aeb522a7ece73d1eb7033ed9cb
dsparkin <daveparkinson49@gmail.com>
2026-09-10 16:24:51 -0700

    Add files via upload

---
+++ Module/src/main/java/Session.java
@@ -0,0 +1,11 @@
+// Represents one mentorship session: an ID, a title, a mentor, a date,
+// a location, and a count of current vs. maximum participants.
+public class Session {
+
+    // TODO: declare fields, constructor, etc.
+
+    public boolean register() {
+        // TODO: implement
+        return false;
+    }
+}
---
+++ Module/src/main/java/SessionList.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+// A singly linked list of mentorship sessions, kept sorted by date
+public class SessionList {
+
+    // TODO: implement SessionList class with required methods
+
+}
+
+// One node in the linked list -- given complete, no need to change
+class Node {
+    Session data;
+    Node next;
+
+    Node(Session data) {
+        this.data = data;
+        this.next = null;
+    }
+}
