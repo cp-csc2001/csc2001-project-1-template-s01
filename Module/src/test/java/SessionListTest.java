bf0863ac9bc0440fb67392939f8440f49b65c354
dsparkin <daveparkinson49@gmail.com>
2026-09-10 16:25:37 -0700

    Add files via upload

---
+++ Module/src/test/java/SessionListTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO: write unit tests for SessionList
+class SessionListTest {
+
+    private SessionList sessions;
+
+    @BeforeEach
+    void setUp() {
+        // TODO: build a SessionList here that your tests can reuse
+        sessions = new SessionList();
+    }
+
+    @Test
+    void addSessionTest() {
+        fail("TODO: implement this test");
+    }
+
+    // TODO: write the tests you think SessionList needs
+}
---
+++ Module/src/test/java/SessionTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO: write unit tests for Session
+class SessionTest {
+
+    private Session session;
+
+    @BeforeEach
+    void setUp() {
+        // TODO: construct a Session here that your tests can reuse.
+        // session = new Session(...);
+    }
+
+    @Test
+    void registerSucceedsWhenSpaceAvailable() {
+        fail("TODO: implement this test");
+    }
+
+    // TODO: write the tests you think Session needs
+}
